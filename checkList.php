b3b541ac99daf8c4fe23ca06bd61abcead8739d1
Roldan Aquino <roldanaquinosegura@gmail.com>
2021-07-21 13:33:35 -0500

    vista checklist preparada

--- checkList.php
+++ checkList.php
@@ -1,4 +1,24 @@
+<?php
+    if (isset($_GET['idFecha']) && isset($_GET['fecha'])) {
+?>
+<?php include "header.php"; ?>
+<div class="container">
+    <div class="card border-0 shadow my-5">
+        <div class="card-body p-5">
+            <h1 class="font-weight-light">Agregar listado de tareas</h1>
+            <p class="lead">
+            </p>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
+<script>
+    $(document).ready(function(){
+        //$('#tablaFechas').load('tablaFechas.php');
+    });
+</script>
 <?php 
-
-    print_r($_GET);
+    } else {
+        header("location:index.php");
+    } 
 ?>
