5ed8245a65c4386934fec59edb1722b37301adff
Roldan Aquino <roldanaquinosegura@gmail.com>
2021-07-21 11:42:35 -0500

    agregado de plantilla

--- header.php
+++ header.php
@@ -7,7 +7,25 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <link rel="stylesheet" href="public/css/style.css">
+    <title>Mi CheckList</title>
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light static-top mb-5 shadow">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">Sistemas Web</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php"> Inicio
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php include "header.php"; ?>
 
-    
+<div class="container">
+<div class="card border-0 shadow my-5">
+    <div class="card-body p-5">
+        <h1 class="font-weight-light">Agregar Fecha de listado</h1>
+        <p class="lead"></p>
+    </div>
+</div>
+</div>
 
 <?php include "footer.php" ?>
